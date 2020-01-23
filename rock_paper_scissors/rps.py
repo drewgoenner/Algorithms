af1f60d81bb474aedf0f8bf82571efb76fbed869
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2020-01-23 08:27:11 -0600

    already got rps working, trying to figure out cache so it works with larger n without lagging

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,20 +2,31 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  options = ['rock', 'paper', 'scissors']
-  all_combos = []
+def rock_paper_scissors(n, cache = None):
+  options = [['rock'], ['paper'], ['scissors']]
+  combos = []
+  
+  if n == 0:
+    return [[]]
+  if n == 1:
+    return options
+  elif cache and cache[n] > 1:
+    return cache[n]
+  else:
+    if not cache:
+      cache = {i: 0 for i in range(n - 1)}
+    cache[n] = rock_paper_scissors(n-1, cache)
+    return cache[n]
 
-  def pair_options(pairs_left, total):
-  	if pairs_left == 0:
-  		all_combos.append(total)
-  		return
+  
+  # arr = rock_paper_scissors(n - 1)
+  # for sub in arr:
+  #   for throw in options:
+  #     new_throw = sub + throw
+  #     combos.append(new_throw)
+  # return combos
 
-  	for i in range(0, len(options)):
-  		pair_options(pairs_left -1, total + [options[i]])
 
-  	pair_options(n, [])
-  	return all_combos
 
 
 if __name__ == "__main__":
