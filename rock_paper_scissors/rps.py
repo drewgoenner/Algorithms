7d68876fac7c3cc3b33e1de5f3db4014959f007f
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2020-01-23 08:39:44 -0600

    got rps working

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,29 +2,35 @@
 
 import sys
 
-def rock_paper_scissors(n, cache = None):
+def rock_paper_scissors(n):
   options = [['rock'], ['paper'], ['scissors']]
-  combos = []
-  
   if n == 0:
     return [[]]
   if n == 1:
     return options
-  elif cache and cache[n] > 1:
-    return cache[n]
-  else:
-    if not cache:
-      cache = {i: 0 for i in range(n - 1)}
-    cache[n] = rock_paper_scissors(n-1, cache)
-    return cache[n]
+  
+  combos = []
+  arr = rock_paper_scissors(n - 1)
+  for sub in arr:
+    for throw in options:
+      new_throw = sub + throw
+      combos.append(new_throw)
+  return combos
+  
+  # options = {1:['rock'], 2:['paper'], 3:['scissors']}
+  # if n == 0:
+  #   return 0
+  # if n == 1:
+  #   return options
+  # elif cache and cache[n] > 1:
+  #   return cache[n]
+  # else:
+  #   if not cache:
+  #     cache[n] = rock_paper_scissors(n, cache)
+  #   return cache[n]
 
   
-  # arr = rock_paper_scissors(n - 1)
-  # for sub in arr:
-  #   for throw in options:
-  #     new_throw = sub + throw
-  #     combos.append(new_throw)
-  # return combos
+ 
 
 
 
